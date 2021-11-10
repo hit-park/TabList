d28a3c59d61f1cae4f4ea86c5644c688ef9d7cbc
hit-park <pikaht@nate.com>
2021-11-11 00:28:58 +0900

    alamofire swift manager package 설치

--- TabList/ViewController.swift
+++ TabList/ViewController.swift
@@ -6,12 +6,27 @@
 //
 
 import UIKit
+import Alamofire
 
 class ViewController: UIViewController {
+    
+    let URL = "https://joy.yanolja.com/v6-6/leisure/categories/v2"
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let af = AF.request(URL, method: .get)
+        af.responseJSON { res in
+            switch res.result {
+            case .success(let value):
+                if let jsonObj = value as? [Dictionary<String, Any>] {
+                    print(jsonObj)
+                }
+            case .failure(_):
+                break
+            }
+        }
+        
         view.backgroundColor = .red
     }
 
